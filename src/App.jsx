68f034772e705eb88ac26a5c9fb389f0c322f159
omer <peynirciomerfaruk@gmail.com>
2022-04-27 16:17:47 +0200

    colors added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@
 import './App.scss'
 
 function App() {
- 
+ const colors=["red","green","yellow", "orange"];
   return (
     <div className="App">
+    <h1>Info Site</h1>
+    <p>There are {colors.length} colors:</p>
+    {colors.map((color,index)=>{
+      return <li key={index}>{color}</li>
+    })}
     </div>
   )
 }
